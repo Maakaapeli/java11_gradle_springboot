49950796ee5cf81ba85d9d144bb2a89d71360662
Jani Oksanen <oksanen.jani@outlook.com>
2021-09-28 21:21:15 +0300

    init

---
+++ src/test/java/fi/maakaapeli/java11/gradle/ControllerTest.java
@@ -0,0 +1,12 @@
+package fi.maakaapeli.java11.gradle;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ControllerTest {
+    @Test
+    void shouldReturn_greeting() {
+        assertTrue(true);
+    }
+}
